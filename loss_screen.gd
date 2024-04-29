79433f9c66951ecb848c244ac84684b95af9cf89
malthelade <ladefogedmalthe@gmail.com>
2024-04-29 08:08:40 +0200

    fjernede tomme linjer

--- loss_screen.gd
+++ loss_screen.gd
@@ -3,17 +3,6 @@ extends Control
 @onready var upgrades = get_node("/root/Upgrades")
 @onready var gamestate = get_node("/root/Gamestate")
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
-
 func _on_button_pressed():
 	upgrades.reset_upgrades()
 	gamestate.reset_gamestate()
